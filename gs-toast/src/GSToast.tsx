b9c412860017001a7fa75c5977588cc117afbeb4
Carlos Braga <243153096+carlos-gs99@users.noreply.github.com>
2025-12-03 16:44:55 +0000

    fix to gs-toast

--- gs-toast/src/GSToast.tsx
+++ gs-toast/src/GSToast.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import clsx from 'clsx';
 import { useTranslation } from '@carlos-gs99/hooks';
